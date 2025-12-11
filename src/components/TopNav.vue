94da307edc5d81a04eb452d391f3ddc4b634a5bf
Victor Glasser <glas0069@algonquinlive.com>
2025-12-10 22:43:56 -0500

    Changed nav background to yellow

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #0049c8d7;
+  background-color: #f2ff00d7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
