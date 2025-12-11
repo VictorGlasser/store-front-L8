6588dcf56a498c42b1941eceaf2d036683cc9ab9
Victor Glasser <glas0069@algonquinlive.com>
2025-12-10 22:46:09 -0500

    Changed nav background to blue

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #f2ff00d7;
+  background-color: #bec800d7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
