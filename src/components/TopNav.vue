7a0de307b20af6adb929211a96bdca0bd0b58388
Victor Glasser <glas0069@algonquinlive.com>
2025-12-10 22:04:51 -0500

    returned color change to normal for demo

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #00c82fd7;
+  background-color: #0049c8d7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
